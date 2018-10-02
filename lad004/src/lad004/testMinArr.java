58a2d6889dfeea7b0c001d7e839068ebb36d717e
caovanhien3007 <43164958+caovanhien3007@users.noreply.github.com>
2018-10-02 13:34:58 +0700

    Update testMinArr.java

--- lad004/src/lad004/testMinArr.java
+++ lad004/src/lad004/testMinArr.java
@@ -22,5 +22,68 @@ class testMinArr {
 	        int result = tes.minArr(arr, n);
 	        assertEquals(expResult, result);
 	}
+	@Test2
+void test() {
 
+		lab04 tes=new lab04();
+		 int n = 5;
+	        int arr[] = new int[n] ;
+	        arr[0]=4;
+	        arr[1]=34;
+	         arr[2]=33;
+	         arr[3]=56;
+	         arr[4]=23;
+	       
+	        int expResult = 4;
+	        int result = tes.minArr(arr, n);
+	        assertEquals(expResult, result);
+	}
+	@Test3
+	void test() {
+
+		lab04 tes=new lab04();
+		 int n = 5;
+	        int arr[] = new int[n] ;
+	        arr[0]=45;
+	        arr[1]=32;
+	         arr[2]=31;
+	         arr[3]=56;
+	         arr[4]=23;
+	       
+	        int expResult = 23;
+	        int result = tes.minArr(arr, n);
+	        assertEquals(expResult, result);
+	}
+	@test4
+	void test() {
+
+		lab04 tes=new lab04();
+		 int n = 5;
+	        int arr[] = new int[n] ;
+	        arr[0]=45;
+	        arr[1]=35;
+	         arr[2]=39;
+	         arr[3]=56;
+	         arr[4]=213;
+	       
+	        int expResult = 35;
+	        int result = tes.minArr(arr, n);
+	        assertEquals(expResult, result);
+	}
+	@Test5
+	void test() {
+
+		lab04 tes=new lab04();
+		 int n = 5;
+	        int arr[] = new int[n] ;
+	        arr[0]=45;
+	        arr[1]=34;
+	         arr[2]=31;
+	         arr[3]=56;
+	         arr[4]=123;
+	       
+	        int expResult = 31;
+	        int result = tes.minArr(arr, n);
+	        assertEquals(expResult, result);
+	}
 }
