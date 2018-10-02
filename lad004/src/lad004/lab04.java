47ff44f106f50451f44614b38d598509e574d29e
caovanhien3007 <43164958+caovanhien3007@users.noreply.github.com>
2018-10-02 14:44:50 +0700

    Update lab04.java

--- lad004/src/lad004/lab04.java
+++ lad004/src/lad004/lab04.java
@@ -1,38 +1,29 @@
-package lad004;
-import java.util.Scanner;  
+package lab04;
+
 public class lab04 {
-	 public static int max(int a,int b)
-	    {
-	        if(b>a)a=b;
-	        return a;
-	    }   
-	    public static int minArr(int arr[],int n)
-	    {
-	        int min=arr[0];
-	        for(int i=1;i<n;i++)
-	        {
-	            if(arr[i]<min)min=arr[i];
-	        }
-	        return min;
-	    }
-	
-    public static String Bmi(double cannang ,double chieucao)
+	public static int max(int a,int b)
     {
-        double BMI=cannang/(chieucao*chieucao);
-        String tmp=new String();
-        if(BMI<18.5)tmp="thieucan";
-        if(BMI>=18.5 && BMI<23)tmp="binhthuong";
-        if(BMI>=23 && BMI <25)tmp="thua can";
-        if(BMI>=25)tmp="beo phi";
-        return tmp;
+        if(b>a)a=b;
+        return a;
+    }   
+    public static int minArr(int arr[],int n)
+    {
+        int min=arr[0];
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i]<min)min=arr[i];
+        }
+        return min;
     }
-	    public static void main(String[] args)
-	    {
-	        Scanner nhap = new Scanner(System.in);
-	        System.out.println("nhap a va b");
-	        int a = nhap.nextInt();
-	        int b = nhap.nextInt();
-	        System.out.println(max(a,b));
-	        
-	    }
+
+public static String bmi(double cannang ,double chieucao)
+{
+    double BMI=cannang/(chieucao*chieucao);
+    String ketqua=new String();
+    if(BMI<18.5)ketqua="thieu can";
+    if(BMI>=18.5 && BMI<23)ketqua="binh thuong";
+    if(BMI>=23 && BMI <25)ketqua="thua can";
+    if(BMI>=25)ketqua="beo phi";
+    return ketqua;
+}
 }
