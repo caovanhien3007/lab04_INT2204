d7b0dd012807fedfda1da8c248f23eb594184924
caovanhien3007 <43164958+caovanhien3007@users.noreply.github.com>
2018-10-02 13:48:25 +0700

    Update testbmi.java

--- lad004/src/lad004/testbmi.java
+++ lad004/src/lad004/testbmi.java
@@ -13,7 +13,55 @@ class testbmi {
 		 double cannang = 5;
 	        double chieucao=
 	       
-	        str expResult = 3;
+	        String expResult = 3;
+	        int result = tes.bmi(cannang, chieucao);
+	        assertEquals(expResult, result);
+	
+	}
+	@Test2
+	void test() {
+
+		lab04 tes=new lab04();
+		 double cannang = 5;
+	        double chieucao=
+	       
+	        String expResult = 3;
+	        int result = tes.bmi(cannang, chieucao);
+	        assertEquals(expResult, result);
+	
+	}
+	@Test3
+	void test() {
+
+		lab04 tes=new lab04();
+		 double cannang = 5;
+	        double chieucao=
+	       
+	        String expResult = 3;
+	        int result = tes.bmi(cannang, chieucao);
+	        assertEquals(expResult, result);
+	
+	}
+	@Test4
+	void test() {
+
+		lab04 tes=new lab04();
+		 double cannang = 5;
+	        double chieucao=
+	       
+	        String expResult = 3;
+	        int result = tes.bmi(cannang, chieucao);
+	        assertEquals(expResult, result);
+	
+	}
+	@Test5
+	void test() {
+
+		lab04 tes=new lab04();
+		 double cannang = 5;
+	        double chieucao=
+	       
+	        String expResult = 3;
 	        int result = tes.bmi(cannang, chieucao);
 	        assertEquals(expResult, result);
 	
